2beb7aa89d5778ff7d6c3ad722ee97ef8f939db5
SarathKalarikkal <sarathkalarikkal0@gmail.com>
2024-09-14 10:00:43 +0530

    added server api

--- client/src/config/axiosInstance.js
+++ client/src/config/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-    baseURL: `${import.meta.env.VITE_BASE_URL}/api/v1`,
+    baseURL: `${import.meta.env.VITE_BASE_URL}api/v1`,
     headers: {
         'Content-Type': 'application/json',
     },
