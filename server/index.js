29af00b2fdc3c62f635ed260328e5af18ead8600
SarathKalarikkal <sarathkalarikkal0@gmail.com>
2024-09-16 21:26:32 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware setup
 app.use(cors({
-    origin: ["https://rent-acar-client.vercel.app", "http://localhost:5173"],
+    origin: ["https://rent-acar-client.vercel.app/", "http://localhost:5173"],
     // origin: "http://localhost:5173",
     credentials: true
 }));
